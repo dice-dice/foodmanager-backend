787a655f7036ae9efe43c1d9d7bcb6532bd1758a
disuke <z7daisuke.debzzha@gmail.com>
2026-01-12 09:25:51 +0900

    fix: add @Profile("!test") for GitHub Actions

--- src/main/java/com/spire/fridge/inventory/config/CategoryInitializer.java
+++ src/main/java/com/spire/fridge/inventory/config/CategoryInitializer.java
@@ -4,11 +4,13 @@ import com.spire.fridge.inventory.entity.Category;
 import com.spire.fridge.inventory.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Profile("!test")
 @Component
 public class CategoryInitializer  {
 
