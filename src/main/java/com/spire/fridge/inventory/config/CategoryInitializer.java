e6b721ac4b4891ab5baeb291d20170119c2df217
disuke <z7daisuke.debzzha@gmail.com>
2026-01-13 06:55:28 +0900

    feat: add new categories

--- src/main/java/com/spire/fridge/inventory/config/CategoryInitializer.java
+++ src/main/java/com/spire/fridge/inventory/config/CategoryInitializer.java
@@ -21,7 +21,7 @@ public class CategoryInitializer  {
         if (categoryRepository.count() == 0) {
 
             List<String> defaults = List.of(
-                    "果物","野菜","肉","魚","乳製品","冷凍","日用品","その他"
+                    "果物","野菜","肉","魚","乳製品","冷蔵","冷凍","常温","飲料","日用品","その他"
             );
 
             for (String name : defaults) {
